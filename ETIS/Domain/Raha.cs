1da818571fd77208ee8e83eb3aa1502b8bebe457
Märten Mändla <marman@ttu.ee>
2018-11-04 22:44:45 +0200

    Kolmas iteratsioon

--- ETIS/Domain/Raha.cs
+++ ETIS/Domain/Raha.cs
@@ -17,6 +17,8 @@ namespace ETIS.Domain
 
         public Raha(decimal? vaartus, string valuuta)
         {
+            if (valuuta == null)
+                throw new ArgumentException("Valuuta peab olema määratud", "valuuta");
             Vaartus = Math.Round(vaartus ?? 0, 2);
             Valuuta = valuuta;
         }
@@ -38,11 +40,49 @@ namespace ETIS.Domain
             return Vaartus == other.Vaartus && String.Compare(Valuuta, other.Valuuta, StringComparison.InvariantCultureIgnoreCase) == 0;
         }
 
+        public bool OnMääratud()
+        {
+            var t = !String.IsNullOrWhiteSpace(Valuuta);
+            return t;
+        }
+
         public override bool Equals(object obj)
         {
             return Equals(obj as Raha);
         }
 
         public string Valuuta { get { return _valuuta; } set { if (value is null) _valuuta = ""; else _valuuta = value; } }
+
+        public string Tekstina(string defaultVaartusKuiMaaramata = "", bool koosÜhikuga = true)
+        {
+            if (!OnMääratud())
+                return defaultVaartusKuiMaaramata;
+
+            return koosÜhikuga ? $"{Vaartus:0.00} {Valuuta}" : $"{Vaartus:0.00}";
+        }
+
+        public string ToString(bool koosÜhikuga = true)
+        {
+            return Tekstina("", koosÜhikuga);
+        }
+
+        public override string ToString()
+        {
+            return ToString(true);
+        }
+
+        public static Raha Parse(string rahaString)
+        {
+            if (String.IsNullOrEmpty(rahaString))
+                return RahaHelper.TühiRaha;
+
+            var components = rahaString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            if (components.Length != 2)
+                return RahaHelper.TühiRaha;
+
+            return new Raha(decimal.Parse(components[0]), components[1]);
+        }
+
+
     }
 }
---
+++ ETIS/Extensions/RahaExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETIS.Domain;
+using ETIS.Helpers;
+
+namespace ETIS.Extensions
+{
+    public static class RahaExtensions
+    {
+        public static bool OnMääratud(this Raha raha)
+        {
+            return raha != null && !raha.Equals(RahaHelper.TühiRaha);
+        }
+
+        public static bool OmabVäärtust(this Raha raha)
+        {
+            return OnMääratud(raha) && raha.Vaartus > 0;
+        }
+
+        public static Raha Sum(this IEnumerable<Raha> src, string valuuta = null)
+        {
+            var sum = new Raha(0, valuuta ?? RahaHelper.VaikeValuuta);
+            foreach (var e in src)
+                sum.Vaartus += e.Vaartus;
+
+
+            // ..
+            return sum;
+        }
+    }
+}
